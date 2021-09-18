aac0c6ef9d537ca7b71e2b0701c14cc5c890f324
Dheeraj Soni <dheerajsoni7023@gmail.com>
2021-09-18 20:05:37 +0530

    adding the requests

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,59 +1,9 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
-    useCreateIndex: true
+    useCreateIndex: true,
+    useFindAndModify: false
 })
 
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    }, 
-    email: {
-            type: String,
-            required: true,
-            trim: true,
-            lowercase: true,
-            validate(value) {
-                if(!validator.isEmail(value)){
-                    throw new Error('Email is invalid')
-                }
-            }
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 7,
-        trim: true,
-        validate(value) {
-            if(value.toLowerCase().includes('password')){
-                throw new Error('Password can not contain "Password" ')
-            }
-        }
-    },
-    age: {
-        type: Number,
-        dafault: 0,
-        validate(value) {
-            if(value<0){
-                throw new Error('Age must be a positive number')
-            }
-        } 
-    }
-})
 
-const me = new User ({
-    name: 'Dheeraj Soni',
-    age: 29,
-    email: 'MYEMAIL@GMAIL.COM',
-    password: 'dhee7658'
-})
-
-me.save().then((me) => {
-    console.log(me)
-}).catch((error) => {
-    console.log('Error!', error)
-})
